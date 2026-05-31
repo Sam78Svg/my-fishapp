3387a08666169c7da979f8342dca5b217f0ccfc2
Sam78Svg <kendresangamwama@gmail.com>
2026-05-31 15:49:03 +0530

    insightAdded

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import './index.css';
 import App from './App.jsx';
 
@@ -11,5 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
